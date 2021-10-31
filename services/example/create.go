51a4addbe68151dd4f6112e63435d0ee1f41b5b9
Arif Luthfi <arifluthfi16@gmail.com>
2021-10-31 15:10:13 +0700

    modified service

--- services/example/create.go
+++ services/example/create.go
@@ -1,10 +1,12 @@
 package example
 
 import (
+	"fmt"
 	"github.com/arifluthfi16/gomvcboilerplate/model"
 )
 
 func (s ExampleService) SayHi () string {
+	fmt.Println("RUNNING AN EXAMPLE SERVICE")
 	return "Successfully started the service"
 }
 
